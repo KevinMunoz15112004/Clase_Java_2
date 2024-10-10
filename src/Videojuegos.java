29991fe643ad1513d47ad03ba2090310ebc5b09e
KevinMunoz15112004 <kevinmunoz57898@gmail.com>
2024-10-10 08:51:42 -0500

    Version 2.0

---
+++ src/Videojuegos.java
@@ -0,0 +1,23 @@
+public class Videojuegos {
+    String nombre;
+    String desarrollador;
+    String plataformas;
+    String clasificacion;
+    int anioPublicacion;
+
+    public Videojuegos(String nombre, String desarrollador, String plataformas, String clasificacion, int anioPublicacion) {
+        this.nombre = nombre;
+        this.desarrollador = desarrollador;
+        this.plataformas = plataformas;
+        this.clasificacion = clasificacion;
+        this.anioPublicacion = anioPublicacion;
+    }
+    public void mostrar() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Desarrollador: " + this.desarrollador);
+        System.out.println("Plataformas: " + this.plataformas);
+        System.out.println("Clasificacion: " + this.clasificacion);
+        System.out.println("Año Publicacion: " + this.anioPublicacion);
+        System.out.println("-------------------------------------------");
+    }
+}
